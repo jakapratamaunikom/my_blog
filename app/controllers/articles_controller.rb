70eeb61798f56a8253996d5906434acd1b63147c
Alexey Volobuev <17valexl@gmail.com>
2015-04-19 14:31:06 +0300

    fixes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,5 @@ class ArticlesController < BaseController
   def index
     add_tasty_breadcrumb I18n.t("views.header.blog"), :root_path
     @articles = Article.published
-    @articles += @articles
   end
 end
--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -40,6 +40,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
     process :resize_to_limit => [242, 200]
   end
 
+  version :thumb_740x280 do
+    process :resize_to_limit => [740, 280]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
