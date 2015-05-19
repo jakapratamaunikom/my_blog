4c3c6f2e24a7da47b8cc7a81387eb122f660dd52
Alexey Volobuev <17valexl@gmail.com>
2015-05-19 12:35:22 +0300

    Display tags cloud

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,2 +1,13 @@
 module MainHelper
+  def display_tags_cloud_for(lang)
+    content_tag :ul, class: 'tags' do
+      Tag.send(lang).inject("".html_safe) do |cloud, tag|
+        cloud += content_tag :li do
+                    content_tag :a, href: '#' do
+                      tag.to_s
+                    end
+                  end
+      end
+    end
+  end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ActiveRecord::Base
-  scope :ru, -> { joins(:article_contents).where(article_contents: {lang: :ru}).uniq }
-  scope :en, -> { joins(:article_contents).where(article_contents: {lang: :en}).uniq }
+  scope :ru, -> { joins(:article_contents).where(article_contents: {lang: :ru, published: true}).uniq }
+  scope :en, -> { joins(:article_contents).where(article_contents: {lang: :en, published: true}).uniq }
 
   has_and_belongs_to_many :article_contents
   has_many :articles, through: :article_contents
