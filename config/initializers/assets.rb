6559462e1c2756f3d0e2f293c44ef6b390993693
Alexey Volobuev <17valexl@gmail.com>
2015-06-15 15:53:09 +0300

    fixes

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,16 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.paths << "#{Rails}/vendor/assets/stylesheets"
+Rails.application.config.assets.paths << "#{Rails}/vendor/assets/fonts"
+
+Rails.application.config.assets.precompile += %w( bootstrap/glyphicons-halflings-regular.eot )
+Rails.application.config.assets.precompile += %w( bootstrap/glyphicons-halflings-regular.woff )
+Rails.application.config.assets.precompile += %w( bootstrap/glyphicons-halflings-regular.ttf )
+Rails.application.config.assets.precompile += %w( bootstrap/glyphicons-halflings-regular.svg )
+
 Rails.application.config.assets.precompile += %w( admin.js admin.css)
 Rails.application.config.assets.precompile += ['.svg', '.eot', '.woff', '.ttf', '*.png']
 Rails.application.config.assets.precompile += %w(*.jpg *.jpeg *.gif)
 Rails.application.config.assets.precompile += %w( i18n.js )
+
