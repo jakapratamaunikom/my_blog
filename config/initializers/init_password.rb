054a0301caa11aa6f5bbd50b1afc6d5b774acd96
Alexey Volobuev <17valexl@gmail.com>
2015-06-18 13:14:30 +0300

    change init_password

--- config/initializers/init_password.rb
+++ config/initializers/init_password.rb
@@ -1,3 +1,6 @@
-ENV["ADMIN_PASSWORD"] = "$2a$10$uyNjmgpUeQ2Dt4cYDrNvDuWOeG5foZ4hqT3fEwhR4ZoAB2nQzYbLi" if Rails.env.test? || Rails.env.development? 
-#For test and development is 112233
-#For production is stored in profile file.
+if Rails.env.production? 
+  ENV["ADMIN_PASSWORD"] = "$2a$10$q2CGpurMxlqqGv3zGjCrq.buJOSWNZ/iSI5rgtrmKACcyR0jBB3VC"
+else
+  #For test and development is 112233
+  ENV["ADMIN_PASSWORD"] = "$2a$10$uyNjmgpUeQ2Dt4cYDrNvDuWOeG5foZ4hqT3fEwhR4ZoAB2nQzYbLi" 
+end
