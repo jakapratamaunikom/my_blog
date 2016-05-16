9ade8be62ab70811c04bdd609f9b246c307e43db
Litvinenko <torerolda@gmail.com>
2016-05-16 09:10:52 +0300

    небольшое исправление

--- db/migrate/20160512201002_image_transfer_from_work_content_to_work.rb
+++ db/migrate/20160512201002_image_transfer_from_work_content_to_work.rb
@@ -1,11 +1,11 @@
 class ImageTransferFromWorkContentToWork < ActiveRecord::Migration
   def change
     WorkContent.where.not(image: nil).find_each do |record|
-      if Work.exists?(record.article_id)
-        article = Work.find(record.article_id);
-        article.image = File.new(File.join(Rails.root, '/public' + record.image.url))
-        article.save!
-        article.image.recreate_versions!
+      if Work.exists?(record.work_id)
+        work = Work.find(record.work_id);
+        work.image = File.new(File.join(Rails.root, '/public' + record.image.url))
+        work.save!
+        work.image.recreate_versions!
       end
     end
   end
